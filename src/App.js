fb4a72d02a844e4e1f4d95cb02c60887f823cb4f
Luke <10928675@uvu.edu>
2023-05-02 22:15:38 -0600

    recipe app created

--- src/App.js
+++ src/App.js
@@ -1,23 +1,124 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const [recipes, setRecipes] = useState([]);
+  const [favoriteRecipes, setFavoriteRecipes] = useState([]);
+  const [form, setForm] = useState({
+    id: '',
+    name: '',
+    ingredients: '',
+    instructions: ''
+  });
+
+  function handleInputChange(event) {
+    const { name, value } = event.target;
+    setForm({
+      ...form,
+      [name]: value
+    });
+  }
+
+  function handleAddRecipe(event) {
+    event.preventDefault();
+    const newRecipe = {
+      id: recipes.length,
+      name: form.name,
+      ingredients: form.ingredients,
+      instructions: form.instructions
+    };
+    setRecipes([...recipes, newRecipe]);
+    setForm({
+      id: '',
+      name: '',
+      ingredients: '',
+      instructions: ''
+    });
+  }
+
+  function handleEditRecipe(recipeId) {
+    const editedRecipe = recipes.find(recipe => recipe.id === recipeId);
+    setForm({
+      id: editedRecipe.id,
+      name: editedRecipe.name,
+      ingredients: editedRecipe.ingredients,
+      instructions: editedRecipe.instructions,
+      isFavorite: editedRecipe.isFavorite
+    });
+    handleDeleteRecipe(recipeId);
+  }
+
+  function handleDeleteRecipe(recipeId) {
+    const updatedRecipes = recipes.filter(recipe => recipe.id !== recipeId);
+    setRecipes(updatedRecipes);
+  }
+
+  function handleFavoriteRecipe(recipeId, isFavorite) {
+    if(!isFavorite) {
+      const favoritedRecipe = recipes.find(recipe => recipe.id === recipeId);
+      setFavoriteRecipes([...favoriteRecipes, favoritedRecipe]);
+      const updatedRecipes = recipes.filter(recipe => recipe.id !== recipeId)
+      setRecipes(updatedRecipes);
+    }
+    else {
+      const unfavoritedRecipe = favoriteRecipes.find(recipe => recipe.id === recipeId);
+      setRecipes([...recipes, unfavoritedRecipe]);
+      const updatedFavoriteRecipes = favoriteRecipes.filter(recipe => recipe.id !== recipeId)
+      setFavoriteRecipes(updatedFavoriteRecipes);
+    }
+    
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div class="flex">
+        <div>
+          <h1>Recipes</h1>
+          <ul>
+            {recipes.map(recipe => (
+              <li key={recipe.id}>
+                <h2>{recipe.name}</h2>
+                <p>Ingredients: {recipe.ingredients}</p>
+                <p>Instructions: {recipe.instructions}</p>
+                <button onClick={() => handleEditRecipe(recipe.id)}>Edit</button>
+                <button onClick={() => handleDeleteRecipe(recipe.id)}>Delete</button>
+                <button onClick={() => handleFavoriteRecipe(recipe.id, false)}>Favorite</button>
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div class="ml-4">
+          <h1>Favorite Recipes</h1>
+          <ul>
+            {favoriteRecipes.map(recipe => (
+              <li key={recipe.id}>
+                <h2>{recipe.name}</h2>
+                <p>Ingredients: {recipe.ingredients}</p>
+                <p>Instructions: {recipe.instructions}</p>
+                <button onClick={() => handleFavoriteRecipe(recipe.id, true)}>Unfavorite</button>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+      <h2>Add Recipe</h2>
+      <form onSubmit={handleAddRecipe}>
+        <label>
+          Name:
+          <input type="text" name="name" value={form.name} onChange={handleInputChange} />
+        </label>
+        <br />
+        <label>
+          Ingredients:
+          <textarea name="ingredients" value={form.ingredients} onChange={handleInputChange} />
+        </label>
+        <br />
+        <label>
+          Instructions:
+          <textarea name="instructions" value={form.instructions} onChange={handleInputChange} />
+        </label>
+        <br />
+        <button type="submit">Add Recipe</button>
+      </form>
     </div>
   );
 }
